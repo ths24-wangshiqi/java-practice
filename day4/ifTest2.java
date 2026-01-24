fab7394777fcfbd2128bf2adc30c428b148690b9
あかしん <se71se71se71@gmail.com>
2026-01-24 22:26:45 +0900

    Pay tast2

---
+++ day4/ifTest2.java
@@ -0,0 +1,16 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ifTest2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("払ってください");
+        int actualPay = sc.nextInt();
+        if (actualPay >= 600) {
+            System.out.println("支払い完了");
+        }else 
+            System.out.println("支払い失敗");
+
+    }
+}
