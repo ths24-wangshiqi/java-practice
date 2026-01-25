0dc5fd59f3e5260958cbb59296fdfecd5da96bb9
あかしん <se71se71se71@gmail.com>
2026-01-25 13:16:05 +0900

    testScores

---
+++ day4/ifTest4.java
@@ -0,0 +1,29 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ifTest4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("テスト点数を入力してください");
+        int testScores = sc.nextInt();
+
+        if (testScores <= 100 && testScores >= 0) {
+            if (testScores >= 95) {
+                System.out.println("自転車プレゼント");
+
+            }else if(testScores >= 90) {
+                 System.out.println("遊園地行く");   
+                
+            }else if(testScores >= 80) {
+                 System.out.println("オモチャプレゼント");   
+                
+            }else{
+                System.out.println("なにもない");
+            }
+                     
+        }else{
+            System.out.println("成績は正常ではない");
+        }
+    }       
+}
