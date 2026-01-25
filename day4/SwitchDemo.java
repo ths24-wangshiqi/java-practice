08def34f3d4a1b072a504d6ad7e187b12122c547
あかしん <se71se71se71@gmail.com>
2026-01-25 14:15:56 +0900

    Switch

--- day4/SwitchDemo.java
+++ day4/SwitchDemo.java
@@ -3,9 +3,24 @@ package day4;
 public class SwitchDemo {
     public static void main(String[] args) {
         //定义变量记录我想吃的面
-        String eat = "兰州拉面";
+        String eat = "尸体";
         //对比
-        
-        //
+        switch (eat) {
+            case "兰州拉面":
+                System.out.println("吃兰州拉面");
+                break;
+            case "武汉热干面":
+                System.out.println("吃武汉热干面");
+                break;
+            case "炸酱面":
+                System.out.println("吃炸酱面");
+                break;
+            case "陕西油泼面":
+                System.out.println("吃陕西油泼面");
+                break;
+            default:
+                System.out.println("吃奥力给");
+                break;
+        }
     }
 }
