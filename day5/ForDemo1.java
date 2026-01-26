ceacf555f015dcbf82b0bf04fa0c402b52372471
あかしん <se71se71se71@gmail.com>
2026-01-26 18:59:16 +0900

    if

---
+++ day5/ForDemo1.java
@@ -0,0 +1,9 @@
+package day5;
+
+public class ForDemo1 {
+    public static void main(String[] args) { 
+        for (int num = 1;num < 10;num++){
+            System.out.println("HelloWorld");
+        }
+    }
+}
---
+++ day5/ForDemo2.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class ForDemo2 {
+    public static void main(String[] args) {
+        for (int num = 1;num <= 5;num++){
+            System.out.println(num);
+        }
+
+        for (int num = 5;num >= 1;num--){
+            System.out.println(num);
+        }
+    }
+}
