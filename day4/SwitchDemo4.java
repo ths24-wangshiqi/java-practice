b62e5a61ef202c861bebfe5fb16485fb1cac80de
あかしん <se71se71se71@gmail.com>
2026-01-26 18:07:03 +0900

    case2

--- day4/SwitchDemo4.java
+++ day4/SwitchDemo4.java
@@ -8,20 +8,9 @@ public class SwitchDemo4 {
         System.out.println("请输入星期数");
         int week = sc.nextInt();
         switch (week) {
-            case 1:  
-            case 2:  
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("工作日");
-                break;
-            case 6:
-            case 7:
-                System.out.println("休息日");
-                break;        
-            default:
-                System.out.println("没有这个星期");
-                break;
+            case 1,2,3,4,5 -> System.out.println("工作日");
+            case 6,7 -> System.out.println("休息日");      
+            default -> System.out.println("没有这个星期");
         }
     }
 }
