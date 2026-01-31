d1f83c7482cc03742ce8ad6a379b18c7b7756ab0
あかしん <se71se71se71@gmail.com>
2026-01-31 22:36:52 +0900

    while2

--- day5/WhileDemo.java
+++ day5/WhileDemo.java
@@ -1,12 +1,15 @@
 public class WhileDemo {
     public static void main(String[] args) {
-        int tal = 8844430;
+        //定义变量
+        double height = 8844430;
         double paper = 0.1;
         int count = 0;
-        while (paper < tal){
+        //循环
+        while (paper < height){
             paper = paper * 2;
-            count = count + 1;
+            count++;
         }
+        //输出
         System.out.println(count);
     }
 }
