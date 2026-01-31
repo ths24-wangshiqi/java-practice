5b6caa0b82a026d57e2213f402c915c916c2e8e6
あかしん <se71se71se71@gmail.com>
2026-02-01 01:07:24 +0900

    InfiniteloopDemo1

---
+++ test/scr/InfiniteloopDemo1.java
@@ -0,0 +1,15 @@
+package test.scr;
+
+public class InfiniteloopDemo1 {
+    public static  void  main(String[] args){
+        //FOR无限循环
+        for(;;){
+            System.out.println("Hello World!");
+        }
+        //while无限循环
+        //while(ture){
+        //    System.out.println("Hello World!");
+        //}
+        
+    }
+}
